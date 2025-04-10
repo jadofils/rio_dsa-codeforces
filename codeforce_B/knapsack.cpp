fcea7bd5ad492eb8cc485d841b8b9fc447692d96
Jado Fils SEZIKEYE <sezikeyejadofils100@gmail.com>
2025-04-10 16:37:23 +0200

    done

--- codeforce_B/knapsack.cpp
+++ codeforce_B/knapsack.cpp
@@ -1,31 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm> // Added missing header for sort function
 using namespace std;
-struct Item{
-    double value,weight;
+
+struct Item {
+    double value, weight;
 };
-bool cmp(Item a, Item b){
+
+bool cmp(Item a, Item b) {
     return a.value/a.weight > b.value/b.weight;
 }
-//fractional knapsack function
-double fractionalKnapsack(int W, Item arr[], int n){
+
+// fractional knapsack function
+double fractionalKnapsack(int W, Item arr[], int n) {
     sort(arr, arr+n, cmp);
     double res = 0.0;
-    for(int i=0;i<n;i++){
-        if(W>arr[i].weight){
+    for(int i=0; i<n; i++) {
+        if(W >= arr[i].weight) { // Fixed comparison (was W > arr[i].weight)
             W -= arr[i].weight;
             res += arr[i].value;
         }
-        else{
-            res += arr[i].value*W/arr[i].weight;
+        else {
+            res += arr[i].value * W / arr[i].weight;
             break;
         }
     }
     return res;
 }
-int main(){
+
+int main() {
     int W = 50; // Maximum weight of knapsack
     Item arr[] = {{60, 10}, {100, 20}, {120, 30}}; // Value and weight of items
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Maximum value in Knapsack = " << fractionalKnapsack(W, arr, n) << endl;
     return 0;
 }
